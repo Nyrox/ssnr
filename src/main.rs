ddbbc824495b207d5f79d522296b6a315f6b4256
Nyrox <mark.oliver@hotmail.de>
2017-11-16 08:08:48 +0100

    Implemented config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate toml;
+
 use std::env;
 use std::process::Command;
 
@@ -6,6 +8,50 @@ mod client;
 mod api;
 
 use client::Client;
+
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Clone, Default)]
+struct Config {
+	default_host: String,
+	default_port: i64
+}
+
+impl Config {
+	pub fn load() -> Config {
+		let mut config = Config::default();
+		
+		let mut config_dir = std::env::home_dir().unwrap();
+		config_dir.push(".config/ssnr");
+		fs::create_dir_all(config_dir.clone());
+		let config_file = { config_dir.push("config.toml"); config_dir };		
+		
+		if let Ok(mut file) = File::open(config_file) {
+			let mut buffer = String::new();
+			file.read_to_string(&mut buffer);
+			let toml = buffer.parse::<toml::Value>().unwrap();
+			println!("{:?}", toml);
+			
+			match toml.get("default_host") {
+				Some(&toml::Value::String(ref host)) => { config.default_host = host.clone(); }
+				Some(value) => { println!("Config parsing error: Found default_host property in config file, but it's value could not be parsed. Expected value::String, found: {:?}", value)}
+				_ => ()
+			}
+			
+			match toml.get("default_port") {
+				Some(&toml::Value::Integer(port)) => { config.default_port = port; }
+				Some(value) => { println!("Config parsing error: Found default_port property in config file, but it's value could not be parsed. Expected value::Integer, found: {:?}", value)}
+				_ => ()
+			}
+		}
+		
+		config
+	}
+}
+
+
 
 fn push(id: &str) {
 	let mut client = Client::connect("127.0.0.1:8542");
@@ -18,6 +64,9 @@ fn pull(id: &str) {
 }
 
 fn main() {
+	let config = Config::load();
+	println!("{:?}", config);
+	
 	for argument in env::args() {
 		println!("{}", argument);
 	}
